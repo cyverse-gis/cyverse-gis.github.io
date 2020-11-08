892d8218a9aa97dc072f0a1f5d34886780a0fd9e
Tyson L. Swetnam <tyson-swetnam@users.noreply.github.com>
2020-11-08 10:10:57 -0700

    Update Navbar.js

--- views/Navbar/Navbar.js
+++ views/Navbar/Navbar.js
@@ -3,9 +3,9 @@ import { View } from '../../node_modules/uki/dist/uki.esm.js';
 class Navbar extends View {
   constructor (d3el) {
     super(d3el, [
-      { type: 'text', url: 'views/Navbar/template.html' },
-      { type: 'json', url: 'views/Navbar/pages.json' },
-      { type: 'less', url: 'views/Navbar/style.less' }
+      { type: 'text', url: '/calliope/views/Navbar/template.html' },
+      { type: 'json', url: '/calliope/views/Navbar/pages.json' },
+      { type: 'less', url: '/calliope/views/Navbar/style.less' }
     ]);
   }
   isCurrentPath (path) {
