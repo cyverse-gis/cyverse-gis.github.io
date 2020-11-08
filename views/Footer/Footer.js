9b7f3871a9d4519419cff0002b2973252039bbf4
Tyson L. Swetnam <tyson-swetnam@users.noreply.github.com>
2020-11-08 10:14:17 -0700

    Update Footer.js

--- views/Footer/Footer.js
+++ views/Footer/Footer.js
@@ -1,10 +1,10 @@
-import { View } from '../../node_modules/uki/dist/uki.esm.js';
+import { View } from '../../../node_modules/uki/dist/uki.esm.js';
 
 class Footer extends View {
   constructor (d3el) {
     super(d3el, [
-      { type: 'text', url: 'views/Footer/template.html' },
-      { type: 'less', url: 'views/Footer/style.less' }
+      { type: 'text', url: '/calliope/views/Footer/template.html' },
+      { type: 'less', url: '/calliope/views/Footer/style.less' }
     ]);
   }
   setup () {
